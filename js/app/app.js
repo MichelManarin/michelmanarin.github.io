31145179505346d78b1894979b95898caa33e919
Michel Manarin <MichelManarin@gmail.com>
2018-04-14 15:40:33 -0300

    Adicionando criação de usuario

--- js/app/app.js
+++ js/app/app.js
@@ -53,3 +53,10 @@ function isLogged(){
             }
         });
 }
+
+function createUser(email,password){
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+}
