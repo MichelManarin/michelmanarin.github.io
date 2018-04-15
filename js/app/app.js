408190c89cc03a2a88506530bb1f07920b364289
Michel Manarin <MichelManarin@gmail.com>
2018-04-15 14:22:54 -0300

    Adicionando logoff

--- js/app/app.js
+++ js/app/app.js
@@ -36,6 +36,18 @@ function callLogin(user,password){
     
 }
 
+function logoff (e){
+
+    e.preventDefault();
+    
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+
+}
+
 function isLogged(){
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser){
