ee65127a61e59ec84ded98dfe35cfdfbe67cf36d
Michel Manarin <MichelManarin@gmail.com>
2018-05-06 14:51:43 -0300

    Subindo implementação de adicionar maquina

--- Atividade04/js/app/controllers/MaquinaCadastroController.js
+++ Atividade04/js/app/controllers/MaquinaCadastroController.js
@@ -2,72 +2,41 @@ class MaquinaController {
     
     constructor() {
 
-
         this._inputNome       = $('#camponome');
         this._inputStatus     = $('#campostatus');
         this._inputMarca      = $('#campomarca');
+        this._inputTipo       = $('#campotipo');
         this._inputInclusao   = $('#campoinclusao');
         this._inputDescricao  = $('#campodescricao');
         this._inputFabricacao = $('#campofabricacao');
-
-        this._loading = $('#loading');
-
-        
-        
-        $( this._loading ).css({ 'display' : 'none' });
+       
+        $('#loading').css({'display':'none'});
 
         this._listaMaquinas = new ListaMaquinas();
         this._maquinasView  = new MaquinasCadastroView($('#maquinaView'));
         this._maquinasView.update(this._listaMaquinas);
-        
 
     }
 
     afterRead(pessoas){
         $( this._showLoading ).css({ 'display' : 'none' });
     }
-    
-    setInfoCard(){
-        this._cardContadorPessoas.textContent = this._listaPessoas.getCount();
-        this._cardMediaIdade.textContent = (this._listaPessoas.getAverage()).toFixed(2);
-    }
-    
-
-    buscarDadosBD(){
-        this._loading.removeAttribute("style");
-        ConexaoBanco.lerPessoas();
-    }
-
-    carregar(pessoas){
-
-        for (var pessoa in pessoas){
-            var obj = pessoas[pessoa];
-            obj.datanasc = new Date(obj.datanasc);
-            this._listaPessoas.adiciona(obj);
-        } ;
-
-        this._pessoasView.update(this._listaPessoas);
-        this._loading.style.width='display:none';
-
-    }
-
-
+        
     adiciona(e) {
         
         e.preventDefault();
 
-        this._listaPessoas.adiciona(this._criaPessoa());
-        this._pessoasView.update(this._listaPessoas);
-        this.setInfoCard(); 
+        alert(adicionou);
+        this._listaMaquinas.adiciona(this._criaMaquina());
+        this._maquinasView.update(this._listaMaquinas);
     }
     
-    _criaPessoa() {
+    _criaMaquina() {
         
-        return new Pessoa(
-            this._inputNome.value,
-            DateHelper.textoParaData(this._inputDataNasc.value),
-            this._inputEmail.value,
-            this._inputTelefone.value);    
+        return new Maquina(this._inputNome, this._inputDescricao, this._inputPessoa, 
+                           this._inputTipo, DateHelper.textoParaData(this._inputFabricacao) , this._inputMarca,
+                           null);
+                           
     }
     
     limpaFormulario() {
